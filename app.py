05decabfbd0efb986260bc1572427595e3d47a60
dilomapo <diana.marin@mashumanoconsultores.com>
2021-12-10 07:06:56 -0500

    Upload archivos back
    Se suben ficheros .py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect
+from persistencia import guardar_pedido
+
+app = Flask (__name__)
+
+@app.route("/pizza",methods=['POST'])
+def pizza():
+    nombre = request.form.get("nm")
+    apellido = request.form.get("ap")    
+    print("Nombre: "+nombre)
+    print("Apellido: "+apellido)
+
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write("")
+        file.close()
+        guardar_pedido(nombre, apellido)
+
+    return redirect("http://localhost/EJERCICIOS/Final/solicita_pedido.html", code=302)
+    
