04c6cbc4074b8d86cf36a5e3b334482d46ed966c
dilomapo <diana.marin@mashumanoconsultores.com>
2021-12-23 14:59:15 -0500

    Ajuste calidad III

--- persistencia.py
+++ persistencia.py
@@ -4,7 +4,7 @@ Persistencia
 def guardar_pedido(nombre, apellidos):
     """
     Guardar Pedido
-    """ 
+    """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
