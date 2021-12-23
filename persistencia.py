f0e82b378668254a010e099f26a9be90d58e3223
dilomapo <diana.marin@mashumanoconsultores.com>
2021-12-23 15:41:05 -0500

    Ajuste guion

--- persistencia.py
+++ persistencia.py
@@ -6,5 +6,5 @@ def guardar_pedido(nombre, apellidos):
     Guardar Pedido
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-"+ nombre + " " + apellidos + "\n")
         file.close()
